4eb6caf1de8b683389c525e3a9fae1990ecd8ac5
KkabyDol <rlaskarb0321@gmail.com>
2023-04-07 23:55:59 +0900

    NormalMonster 세팅
    * NormalMonster 세팅
    1.  인식범위내에 플레이어가 들어오면 타겟이 설정됨 상태로 변함, 타겟이 설정됨상태가 아니라면 그저 정찰만한다.
    2. 만일 타겟이 설정된 상태라면 쫓거나 공격을하게된다.
    
    + 해야하는 일
    1. 공격을하고 잠시후에 공격상태를 탈출
    2. 추격상태일때 v3 distance를 사용해서 일정거리이하가되면 attack으로 전환시키기
    3. NonKeyMonster, KeyMonster 스크립트 삭제시킴, 굳이 그렇게 나눠야할필요가 없다고느낌. 그냥 키몹처럼만드려면 이벤트때 추격범위를 엄청넓게 설정한 몬스터들을 소환시키면 되니까

--- Assets/02.Scripts/Monster/Monster.cs
+++ Assets/02.Scripts/Monster/Monster.cs
@@ -1,17 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public struct MonsterBasicStat
 {
-    public int _health;
-    public float _movSpeed;
+    public int _health; // 체력
+    public bool _isAttackFirst; // 선공or비선공여부
+    public float _attackDelay; // 공격후 다음공격까지 기다려야하는 시간값
+    public float _traceRadius; // 추격을 인지하는 범위
+    public float _attakableRadius; // 공격사정거리
 }
 
+/// <summary>
+/// 모든 몬스터들이 가져야하는 기본요소
+/// </summary>
 public class Monster : MonoBehaviour
 {
-    public enum eMonsterLevel { Normal, NormalElite, MiddleBoss, Boss }
-    public eMonsterLevel _monsterLevel;
-    public MonsterBasicStat _stat;
+    public enum eMonsterState { Patrol, Trace, Attack, Dead, }
+    public enum eMonsterLevel { Normal, Elite, MiddleBoss, Boss, }
+
+    [Tooltip("몬스터의 현재 상태를 나타냄")]
+    public eMonsterState _monsterState;
+    
+    [Tooltip("몬스터의 단계, 계급")]
+    public eMonsterLevel _level;
+    
+    [Tooltip("몬스터들 기본적 스텟 요소")]
+    public MonsterBasicStat _monsterBasicStat;
+
+    [Tooltip("몬스터들 다음 행동까지 걸리게 할 시간")]
+    public float _nextActDelay;
+
+    [Header("Target")]
+    [Tooltip("몬스터들의 추적할 적(= 플레이어)")]
+    public Transform _target;
+
+    protected string _playerName;
+
+    public virtual void DecreaseHp(float amount)
+    {
+
+    }
+
+    public virtual void Dead()
+    {
+
+    }
+
+    public virtual IEnumerator DoAttack()
+    {
+        yield return null;
+    }
 }
