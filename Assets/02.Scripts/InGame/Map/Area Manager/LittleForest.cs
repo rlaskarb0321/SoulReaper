f16d37622e9cdabfc79c6a43294bbd33fd952f25
KkabyDol <rlaskarb0321@gmail.com>
2023-09-23 17:17:21 +0900

    플레이어 낙사
    * 플레이어 낙사
    1. 대략 6초 쯤 마다 플레이어의 위치를 저장함.
    2. 낙사 판정이 되면 6초 전 위치로 되돌리고, 낙사하러 떨어지는 도중에는 위치 기록을 하지 않음

---
+++ Assets/02.Scripts/InGame/Map/Area Manager/LittleForest.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleForest : MonoBehaviour
+{
+    [Header("=== Fall Death ===")]
+    [SerializeField] private PlayerFSM _player;
+    [SerializeField] private GameObject _camLookAt;
+    [SerializeField] private GameObject _fadePanel;
+    [Range(1.0f, 10.0f)] [SerializeField] private float _recordTime;
+    [SerializeField] private Vector3 _reversPos;
+
+    private float _recordTimeOrigin;
+    private bool _isFallDeath;
+
+    private void Start()
+    {
+        _recordTimeOrigin = _recordTime;
+        _reversPos = _player.transform.position;
+    }
+
+    private void Update()
+    {
+        CheckFallDeath();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("PlayerTeam"))
+            return;
+
+        _player.transform.position = _reversPos;
+        _isFallDeath = true;
+    }
+
+    private void CheckFallDeath()
+    {
+        if (_player.State == PlayerFSM.eState.Fall)
+        {
+            _recordTime = _recordTimeOrigin;
+            return;
+        }
+
+        if (_isFallDeath || _recordTime < 0.0f)
+        {
+            _reversPos = _player.transform.position;
+            _recordTime = _recordTimeOrigin;
+            _isFallDeath = false;
+        }
+
+        _recordTime -= Time.deltaTime;
+    }
+}
