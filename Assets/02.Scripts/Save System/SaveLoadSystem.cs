1d50cceac573dc317cf33c9302634c122c3acc27
KkabyDol <rlaskarb0321@gmail.com>
2023-10-23 23:14:40 +0900

    저장 & 불러오기 구조 짜는 중
    * 저장 & 불러오기
    1. 테스트로 string[20000] 가져왔을 때 & 저장할 때 렉이 걸리나 시험, 눈에 띄게 걸리지 않음

---
+++ Assets/02.Scripts/Save System/SaveLoadSystem.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using UnityEngine;
+
+public static class SaveLoadSystem
+{
+    private static string SavePath => Application.persistentDataPath + "/saves/";
+
+    public static void Save(SaveData data, string saveFileName)
+    {
+        if (!Directory.Exists(SavePath))
+        {
+            Directory.CreateDirectory(SavePath);
+        }
+
+        string saveJson = JsonUtility.ToJson(data);
+        string saveFilePath = SavePath + saveFileName + ".json";
+
+        File.WriteAllText(saveFilePath, saveJson);
+        Debug.Log("Save Success: " + saveFilePath);
+    }
+
+    public static SaveData Load(string saveFileName)
+    {
+        string saveFilePath = SavePath + saveFileName + ".json";
+
+        if (!File.Exists(saveFilePath))
+        {
+            Debug.LogError("No such saveFile exists");
+            return null;
+        }
+
+        string saveFile = File.ReadAllText(saveFilePath);
+        SaveData saveData = JsonUtility.FromJson<SaveData>(saveFile);
+
+        return saveData;
+    }
+}
---
+++ Assets/02.Scripts/Save System/SaveLoadTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadTest : MonoBehaviour
+{
+    //public string[] _test;
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.B))
+    //    {
+    //        SaveData data = new SaveData();
+    //        SaveLoadSystem.Save(data, "Save_00");
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.L))
+    //    {
+    //        SaveData loadData = SaveLoadSystem.Load("Save_00");
+    //        _test = loadData._testArr;
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.K))
+    //    {
+    //        if (_test == null)
+    //            return;
+
+    //        for (int i = 0; i < _test.Length; i++)
+    //        {
+    //            _test[i] = i.ToString();
+    //        }
+    //    }
+    //}
+}
