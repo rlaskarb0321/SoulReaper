790c7305b86e79fa3cc8e7f2a9d45ced1e5a116c
KkabyDol <68903562+rlaskarb0321@users.noreply.github.com>
2024-03-25 14:35:10 +0900

    Update OneWayDialogMarker.cs
    * OneWayDialogMarker.cs 수정
    - 변수 주석 추가

--- SoulsReaper/Assets/02.Scripts/InGame/Dialog/One Way Talk/OneWayDialogMarker.cs
+++ SoulsReaper/Assets/02.Scripts/InGame/Dialog/One Way Talk/OneWayDialogMarker.cs
@@ -6,6 +6,7 @@ using UnityEngine.Timeline;
 
 public class OneWayDialogMarker : Marker, INotification
 {
+    // 씨네머신 (커스텀)마커를 통해 대화 csv 파일과 레터링 속도를 지정
     public TextAsset _oneWayDialog;
     public float _letteringSpeed;
 
